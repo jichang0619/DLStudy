c33778a89c176cccf3e1e6742b36c1856414979e
JichangKim <jichang0619@gmail.com>
2024-08-10 18:07:11 +0900

    DL From scratch #1

---
+++ part1_5.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+# print(x * y)
+
+A = np.array([[1,2], [3,4]])
+print(A.dtype)
---
+++ part1_6.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib as 
+
+
+#plt.plot(x,y)
+#plt.show()
+
+import sys
+
+print(sys.version_info.major)
+
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
